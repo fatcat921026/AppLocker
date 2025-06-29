a1c0d2587f5815d69b838f251bdc005a17422743
fatcat921026 <xxx921026@gmail.com>
2025-06-29 12:08:41 +0800

    無廣告版本

--- app/src/main/java/com/momentolabs/app/security/applocker/AppLockerApplication.kt
+++ app/src/main/java/com/momentolabs/app/security/applocker/AppLockerApplication.kt
@@ -2,11 +2,11 @@ package com.momentolabs.app.security.applocker
 
 import android.content.Context
 import androidx.multidex.MultiDex
-import com.bugsnag.android.Bugsnag
-import com.facebook.FacebookSdk
-import com.facebook.soloader.SoLoader
+// import com.bugsnag.android.Bugsnag  // 移除 - 錯誤回報
+// import com.facebook.FacebookSdk     // 移除 - Facebook SDK
+// import com.facebook.soloader.SoLoader  // 移除 - Facebook SoLoader
 import com.facebook.stetho.Stetho
-import com.google.android.gms.ads.MobileAds
+// import com.google.android.gms.ads.MobileAds  // 移除 - Google 廣告
 import com.momentolabs.app.security.applocker.di.component.DaggerAppComponent
 import com.momentolabs.app.security.applocker.service.ServiceStarter
 import com.momentolabs.app.security.applocker.service.worker.WorkerStarter
@@ -21,12 +21,28 @@ class AppLockerApplication : DaggerApplication() {
 
     override fun onCreate() {
         super.onCreate()
-        MobileAds.initialize(this, getString(R.string.mobile_ad_id))
-        Stetho.initializeWithDefaults(this)
-        Bugsnag.init(this)
+        
+        // 移除廣告初始化
+        // MobileAds.initialize(this, getString(R.string.mobile_ad_id))
+        
+        // 保留 Stetho (開發者工具，可選擇移除)
+        if (BuildConfig.DEBUG) {
+            Stetho.initializeWithDefaults(this)
+        }
+        
+        // 移除錯誤回報
+        // Bugsnag.init(this)
+        
+        // 保留日誌工具
         Beaver.build(this)
+        
+        // 保留核心服務
         ServiceStarter.startService(this)
-        SoLoader.init(this, false)
+        
+        // 移除 Facebook SoLoader
+        // SoLoader.init(this, false)
+        
+        // 保留工作排程器
         WorkerStarter.startServiceCheckerWorker()
     }
 
